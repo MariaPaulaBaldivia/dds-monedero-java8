d0fb661d7fdbe0e9c04925756cb2698ad424ffaa
MariaPaulaBaldivia <m.paulabaldivia00@gmail.com>
2021-05-12 23:25:10 -0300

    Corrijo code smell Feature Envy en getMontoExtraidoA()

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -74,7 +74,7 @@ public class Cuenta {
 
   public Double getMontoExtraidoA(LocalDate fecha) {
     return getMovimientos().stream()
-        .filter(movimiento -> !movimiento.isDeposito() && movimiento.getFecha().equals(fecha))
+        .filter(movimiento -> movimiento.fueExtraido(fecha))
         .mapToDouble(Movimiento::getMonto)
         .sum();
   }
